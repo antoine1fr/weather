ea1583d16a8b0ec6f3fc48d4d132219bff9f5f88
Antoine Luciani <antoine1fr@gmail.com>
2014-08-30 19:46:04 +0200

    CI: install opam packages before launching make.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,6 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS=""
+OPAM_PACKAGES=( cohttp core async uri atdgen )
 
 function setup_opam
 {
@@ -20,6 +21,7 @@ function setup_ubuntu_env
   sudo apt-get update -qq
   sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
   setup_opam
+  opam install ${PACKAGES}
   make
   # make test
 }
